b214d26227002fdb8a0f6b4e40dcad90484413ff
Kevin Zhou <691208yz@gmail.com>
2020-11-04 14:27:25 -0600

    shop page added and moving to firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import AuthPage from './pages/auth/auth';
+
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE </h1>
@@ -19,6 +21,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/auth' component={AuthPage} />
       </Switch>
     </div>
   );
