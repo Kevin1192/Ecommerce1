632e1dfc7b0c49b872b75ceca6eeee9c173ebfe6
Kevin Zhou <691208yz@gmail.com>
2020-11-09 10:39:56 -0600

    add reselector for performance

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCart } from '../../redux/action/cart.actions';
+import { selectCartItemsCount } from '../../redux/selector/cart.selector';
 import { ReactComponent as ShoppingIcon} from '../../assets/imgs/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemCount }) => (
     <div className='cart-icon' onClick={toggleCart }>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+<span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -18,4 +19,7 @@ const mapDispatchToProps = dispatch => ({
     toggleCart: () => dispatch(toggleCart())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
